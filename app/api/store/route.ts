0420483a9ccddd954650ba5437e9ad214a533c24
arliannasrul <arliannasrul@gmail.com>
2025-06-05 19:02:15 +0700

    database,prisma,neon

---
+++ app/api/store/route.ts
@@ -0,0 +1,34 @@
+import db from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+
+export async function POST(request: Request) {
+    try {
+        const { userId } = await auth()
+        const body = await request.json()
+
+        const {name} = body
+
+        if (!userId) {
+            return new NextResponse("unauthorized", { status: 401 })
+        }
+
+        if (!name) {
+            return new NextResponse("Nama toko perlu diisi", { status: 400 })
+        }
+
+        const store = await db.store.create({
+            data: {
+                name,
+                userId
+            }
+        })
+
+        return NextResponse.json(store, { status: 201 })
+
+    } catch (error) {
+        console.log("[STORE_POST]", error)
+        return new NextResponse("internal error", { status: 500 })
+    }
+}
