2e495054d68ba88412d132812ca37d2dba2fdde3
arliannasrul <arliannasrul@gmail.com>
2025-07-07 19:32:25 +0700

    fix: remove unused client imports from home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,6 @@
 "use client"
 
-import Image from "next/image";
-import { UserButton } from "@clerk/nextjs";
-import Modal from "@/components/ui/modal";
+
 import { useStoreModal } from "@/hooks/use-store-modal";
 import { useEffect } from "react";
 
