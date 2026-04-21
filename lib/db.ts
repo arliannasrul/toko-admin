7e8ff4743d1a296da49a53ad75efd10a54d0a805
arliannasrul <arliannasrul@gmail.com>
2026-04-22 00:01:37 +0700

    r

--- lib/db.ts
+++ lib/db.ts
@@ -4,9 +4,7 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 
-const db = globalThis.prisma || new PrismaClient({
-  datasourceUrl: process.env.DATABASE_URL,
-});
+const db = globalThis.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
 
